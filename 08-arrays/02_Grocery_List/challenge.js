4b965d745ea41a906e58ba190999d7f4a2b85173
erin <erinlee2007@u.northwestern.edu>
2014-06-06 12:55:55 +0000

    make all array challenges pass

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,12 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(item, list) {
+  if (list.indexOf(item) > -1) {
+    return list;
+  } else {
+    list.push(item);
+    return list;
+  }
+};
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function(arg) {
+  return arg.sort().reverse();
+};

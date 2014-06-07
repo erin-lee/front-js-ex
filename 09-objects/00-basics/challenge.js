6774ceae814e371accb0ca9755864c0f2e2a42fe
erin <erinlee2007@u.northwestern.edu>
2014-06-07 04:40:06 +0000

    make all object challenges pass

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,39 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle, courseDuration, courseStudents) {
+  var course = {};
+  course.title = courseTitle;
+  course.duration = courseDuration;
+  course.students = courseStudents;
+  return course;
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(obj, newProp, newValue) {
+  if (obj.newProp) {
+    return obj;
+  } else {
+    obj[newProp] = newValue;
+    return obj;
+  }
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter)  {
+  var letterArr = []
+  ,  greeting = ""
+  ,  signature = ""
 
-module.exports.canIGet = undefined;
+  greeting = "Hello " + letter.recipient + ",";
+  signature = "Sincerely,\n" + letter.sender;
+  letterArr = [ greeting, letter.msg, signature ];
+  letterStr = letterArr.join("\n\n");
+
+  return letterStr;
+};
+
+module.exports.canIGet = function(item, money) {
+  var appleStuff = {
+    "MacBook Air" : 999,
+    "MacBook Pro" : 1299,
+    "Mac Pro" : 2499,
+    "Apple Sticker" : 1
+  }
+  return appleStuff[item] <= money;
+};
